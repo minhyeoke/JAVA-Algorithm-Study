60cacd123410e55940a2da97a4da5c8fe4eba616
minhyeoke <minhyuke@naver.com>
2022-01-16 20:08:04 +0900

    BAEKJOON ALGORITHM 11720번 숫자의 합

---
+++ src/A2A20220116.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+
+public class A2A20220116 {
+ 
+	public static void main(String[] args) throws IOException
+	{
+		char a;
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		a=br.readLine().charAt(0);
+		
+		bw.write(Integer.toString((int)a));
+		bw.close();
+		br.close();
+	    
+		
+	}
+	
+}
