7267e35d5c1b8bc65177d2aecee7d983a164ad8f
minhyeoke <minhyuke@naver.com>
2022-01-16 19:03:50 +0900

    BAEKJOON ALGORITHM 1065번

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-
-		int sum = 0;
 		int a = 0;
+		int sum = 99;
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		a = Integer.parseInt(br.readLine());
-
-		if (a == 1000)
+		if (a < 100)
+			sum = a;
+		else if (a == 1000)
 			a = 999;
 
-		if (a >= 100) {
-			sum += 99;
-			for (int i = 100; i <= a; i++) {
-				int getdn = getDn(i);
-				if (getdn == 1)
-					sum++;
-
-			}
-			 
-			bw.write(Integer.toString(sum));
-		} else
-			bw.write(Integer.toString(a));
-
+		for (int i = 100; i <= a; i++) {
+			int b = getDn(i);
+			if(b==1)
+				sum++;
+		}
+		bw.write(Integer.toString(sum));
 		bw.close();
-
+		br.close();
 	}
 
 	public static int getDn(int n) {
-		int getdn = 0;
-		int arr[] = new int[3];
-		int i = 0;
-		arr[1] = 0;
-		arr[2] = 0;
-		getdn = n;
+		int result = 0;
+		int one = n % 10;
+		int ten = (n / 10) % 10;
+		int hun = (n / 100) % 10;
 
-		while (n > 0) {
-			arr[i] = n % 10;
-			n = n / 10;
-			i++;
-		}
+		if (ten - one == hun - ten)
+			result = 1;
 
-		if (arr[1] - arr[0] == arr[2] - arr[1]) {
-			return 1;
-		} else
-			return 0;
+		return result;
 
 	}
 }
