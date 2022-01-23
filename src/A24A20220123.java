dab573c2846b08224843d4aa845612b2b1b0b713
Hyeok <97186275+minhyeoke@users.noreply.github.com>
2022-01-23 23:48:58 +0900

    BAEKJOON ALGORITHM 16170번 오늘의 날짜는?

---
+++ src/A24A20220123.java
@@ -0,0 +1,18 @@
+package lsa;
+import java.util.*;
+import java.io.*;
+
+public class A24A20220123 {
+
+	public static void main(String[] args) throws IOException
+	{
+	
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	 
+		bw.write("2022"+"\n"+"01"+"\n"+"23");
+		
+		bw.close();
+		br.close();
+	}
+}
