85498166b1b12c9839d44905c5af8a0dde292f9b
Hyeok <97186275+minhyeoke@users.noreply.github.com>
2022-02-02 23:18:21 +0900

    BAEKJOON ALGORITHM 10926번 ??!

---
+++ src/A33A20220202.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class A33A20220202 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s=br.readLine();
+        bw.write(s+"??!");
+		bw.close();
+		br.close();
+	}
+}
